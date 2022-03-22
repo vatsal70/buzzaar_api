8754ac8ec6786e5a22060d7fd665c07064319a1a
Vatsal Vohera <68066033+vatsal70@users.noreply.github.com>
2022-03-22 14:44:36 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -12,12 +12,12 @@ app.use(cookieParser())
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute")
 const order = require("./routes/orderRoute")
-//const seller = require("./routes/sellerRoute")
+const seller = require("./routes/sellerRoute")
 
 app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1/", order);
-//app.use("/api/v1/", seller);
+app.use("/api/v1/", seller);
 
 //Middleware for Errors
 app.use(errorMiddleware);
